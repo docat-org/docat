51c84c6c8d230c7597a3386dfd2fbd27c1682dc5
Benj Fassbind <randombenj@gmail.com>
2019-11-08 09:49:27 +0100

    feat(web): init vue project

---
+++ web/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="docat logo" src="./assets/logo.png">
+    <ProjectOverview msg="Welcome to docat!"/>
+  </div>
+</template>
+
+<script>
+import ProjectOverview from './components/ProjectOverview.vue'
+
+export default {
+  name: 'app',
+  components: {
+    ProjectOverview
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ web/src/components/ProjectOverview.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProjectOverview',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+</style>
