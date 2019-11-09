d6c07a79aa369112b81efc7da7fdc6d56c645bdb
fliiiix <hi@l33t.name>
2019-11-09 21:54:49 +0100

    feat(backend): tox support for linting

--- backend/app.py
+++ backend/app.py
@@ -1,41 +1,42 @@
 import os
 import tempfile
-from zipfile import ZipFile
-from flask import Flask, request, jsonify, render_template
-from werkzeug.utils import secure_filename
 from subprocess import run
+from zipfile import ZipFile
+
+from flask import Flask, jsonify, render_template, request
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = "/var/docat/doc"
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16MB
+app.config["UPLOAD_FOLDER"] = "/var/docat/doc"
+app.config["MAX_CONTENT_LENGTH"] = 16 * 1024 * 1024  # 16MB
 
 
-@app.route("/api/<project>/<version>", methods=['POST'])
+@app.route("/api/<project>/<version>", methods=["POST"])
 def upload(project, version):
-    if 'file' not in request.files:
-        resp = jsonify({'message': 'No file part in the request'})
+    if "file" not in request.files:
+        resp = jsonify({"message": "No file part in the request"})
         resp.status_code = 400
         return resp
-    file = request.files['file']
+    file = request.files["file"]
 
-    if file.filename == '':
-        resp = jsonify({'message': 'No file selected for uploading'})
+    if file.filename == "":
+        resp = jsonify({"message": "No file selected for uploading"})
         resp.status_code = 400
         return resp
 
     filename = secure_filename(file.filename)
-    file_ext = filename.rsplit('.', 1)[1].lower()
-    base_path = os.path.join(app.config['UPLOAD_FOLDER'], project, version)
+    file_ext = filename.rsplit(".", 1)[1].lower()
+    base_path = os.path.join(app.config["UPLOAD_FOLDER"], project, version)
 
     if not os.path.exists(base_path):
         os.makedirs(base_path)
 
-    if file_ext == 'zip':
+    if file_ext == "zip":
         with tempfile.TemporaryDirectory() as temp_dir:
             zip_path = os.path.join(temp_dir, filename)
             file.save(zip_path)
 
-            with ZipFile(zip_path, 'r') as zipf:
+            with ZipFile(zip_path, "r") as zipf:
                 zipf.extractall(path=base_path)
     else:
         file.save(os.path.join(base_path, filename))
@@ -44,30 +45,30 @@ def upload(project, version):
     nginx_location = "/etc/nginx/locations.d"
     nginx_config = os.path.join(nginx_location, "{}-doc.conf".format(project))
     if not os.path.exists(nginx_config):
-        project_base_path = os.path.join(app.config['UPLOAD_FOLDER'], project)
-        out_parsed_template = render_template("nginx-doc.conf",
-                                              project=project,
-                                              dir_path=project_base_path)
+        project_base_path = os.path.join(app.config["UPLOAD_FOLDER"], project)
+        out_parsed_template = render_template(
+            "nginx-doc.conf", project=project, dir_path=project_base_path
+        )
         with open(nginx_config, "w") as f:
             f.write(out_parsed_template)
 
         run(["sudo", "nginx", "-s" "reload"])
 
-    resp = jsonify({'message': 'File successfully uploaded'})
+    resp = jsonify({"message": "File successfully uploaded"})
     resp.status_code = 201
     return resp
 
 
-@app.route("/api/<project>/<version>/tags/<new_tag>", methods=['PUT'])
+@app.route("/api/<project>/<version>/tags/<new_tag>", methods=["PUT"])
 def tag(project, version, new_tag):
     src = version
-    dst = os.path.join(app.config['UPLOAD_FOLDER'], project, new_tag)
+    dst = os.path.join(app.config["UPLOAD_FOLDER"], project, new_tag)
 
     if os.path.exists(dst):
         os.unlink(dst)
     os.symlink(src, dst)
 
-    msg = 'Tag {} -> {} successfully created'.format(new_tag, version)
-    resp = jsonify({'message': msg})
+    msg = "Tag {} -> {} successfully created".format(new_tag, version)
+    resp = jsonify({"message": msg})
     resp.status_code = 201
     return resp
