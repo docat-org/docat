fd972a766932ab0e2a3522c4f34caa42620cc3e0
Guillaume Mösching <guillaume.mosching@gmail.com>
2022-02-24 22:06:36 +0100

    Revert "(feat) redirection to latest as default"
    This reverts commit c8f94d7d23a9a1c7e9c5583ab7d750e83bce052a.

--- web/src/main.js
+++ web/src/main.js
@@ -59,7 +59,6 @@ const routes = [
   { path: '/upload', component: Upload },
   { path: '/claim', component: Claim },
   { path: '/delete', component: Delete },
-  { path: '/:project', redirect: '/:project/latest' },
 ]
 
 const router = new VueRouter({
