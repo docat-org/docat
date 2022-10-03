f8918fc7ffbf81700aee521b1f4311c6e42b8dc5
reglim <mauro.regli@roche.com>
2022-10-03 08:59:20 +0200

    Fix(docat): Icon-Upload Mime Type Check
    I added Python-Magic to do that. I also had to adapt the tests because
    they succeeded even though the content was text not binary.
    
    Is also required for the new docatl push-icon feature.
    
    fixed: #289

--- docat/docat/app.py
+++ docat/docat/app.py
@@ -14,6 +14,7 @@ from dataclasses import dataclass
 from pathlib import Path
 from typing import Optional
 
+import magic
 from fastapi import Depends, FastAPI, File, Header, Response, UploadFile, status
 from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
@@ -129,7 +130,10 @@ def upload_icon(
         response.status_code = status.HTTP_404_NOT_FOUND
         return ApiResponse(message=f"Project {project} not found")
 
-    if not file.content_type.startswith("image/"):
+    mime_type_checker = magic.Magic(mime=True)
+    mime_type = mime_type_checker.from_buffer(file.file.read())
+
+    if not mime_type.startswith("image/"):
         response.status_code = status.HTTP_400_BAD_REQUEST
         return ApiResponse(message="Icon must be an image")
 
--- docat/tests/test_upload_icon.py
+++ docat/tests/test_upload_icon.py
@@ -1,7 +1,12 @@
+import base64
 import io
 from unittest.mock import call, patch
 
 import docat.app as docat
+
+ONE_PIXEL_PNG = base64.decodebytes(
+    b"iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAA1JREFUGFdjYGBg+A8AAQQBAHAgZQsAAAAASUVORK5CYII="
+)
 
 
 def test_successful_icon_upload(client_with_claimed_project):
@@ -13,7 +18,7 @@ def test_successful_icon_upload(client_with_claimed_project):
     with patch("shutil.copyfileobj") as copyfileobj_mock, patch("os.remove") as remove_file_mock:
         upload_response = client_with_claimed_project.post(
             "/api/some-project/icon",
-            files={"file": ("icon.jpg", io.BytesIO(b"some image data"), "image/jpeg")},
+            files={"file": ("icon.jpg", io.BytesIO(ONE_PIXEL_PNG), "image/png")},
         )
 
         assert upload_response.status_code == 200
@@ -26,7 +31,7 @@ def test_icon_upload_fails_with_no_project(client_with_claimed_project):
     with patch("shutil.copyfileobj") as copyfileobj_mock, patch("os.remove") as remove_file_mock:
         upload_response = client_with_claimed_project.post(
             "/api/non-existing-project/icon",
-            files={"file": ("icon.png", io.BytesIO(b"some image data"), "image/png")},
+            files={"file": ("icon.png", io.BytesIO(ONE_PIXEL_PNG), "image/png")},
         )
 
         assert upload_response.status_code == 404
@@ -49,7 +54,7 @@ def test_icon_upload_fails_no_token_and_existing_icon(client):
     with patch("shutil.copyfileobj") as copyfileobj_mock, patch("os.remove") as remove_file_mock:
         upload_response_1 = client.post(
             "/api/some-project/icon",
-            files={"file": ("icon.png", io.BytesIO(b"some image data"), "image/png")},
+            files={"file": ("icon.png", io.BytesIO(ONE_PIXEL_PNG), "image/png")},
         )
         assert upload_response_1.status_code == 200
         assert upload_response_1.json() == {"message": "Icon successfully uploaded"}
@@ -59,7 +64,7 @@ def test_icon_upload_fails_no_token_and_existing_icon(client):
     with patch("shutil.copyfileobj") as copyfileobj_mock, patch("os.remove") as remove_file_mock:
         upload_response_2 = client.post(
             "/api/some-project/icon",
-            files={"file": ("icon.png", io.BytesIO(b"some other image data"), "image/png")},
+            files={"file": ("icon.png", io.BytesIO(ONE_PIXEL_PNG), "image/png")},
         )
         assert upload_response_2.status_code == 401
         assert upload_response_2.json() == {"message": "Please provide a header with a valid Docat-Api-Key token for some-project"}
@@ -82,7 +87,7 @@ def test_icon_upload_successful_replacement_with_token(client_with_claimed_proje
     with patch("shutil.copyfileobj") as copyfileobj_mock, patch("os.remove") as remove_file_mock:
         upload_response_1 = client_with_claimed_project.post(
             "/api/some-project/icon",
-            files={"file": ("icon.png", io.BytesIO(b"some image data"), "image/png")},
+            files={"file": ("icon.png", io.BytesIO(ONE_PIXEL_PNG), "image/png")},
             headers={"Docat-Api-Key": "1234"},
         )
         assert upload_response_1.status_code == 200
@@ -93,7 +98,7 @@ def test_icon_upload_successful_replacement_with_token(client_with_claimed_proje
     with patch("shutil.copyfileobj") as copyfileobj_mock, patch("os.remove") as remove_file_mock:
         upload_response_1 = client_with_claimed_project.post(
             "/api/some-project/icon",
-            files={"file": ("icon.png", io.BytesIO(b"some other image data"), "image/png")},
+            files={"file": ("icon.png", io.BytesIO(ONE_PIXEL_PNG), "image/png")},
             headers={"Docat-Api-Key": "1234"},
         )
         assert upload_response_1.status_code == 200
@@ -111,7 +116,7 @@ def test_icon_upload_successful_no_token_no_existing_icon(client):
     with patch("shutil.copyfileobj") as copyfileobj_mock, patch("os.remove") as remove_file_mock:
         upload_response = client.post(
             "/api/some-project/icon",
-            files={"file": ("icon.png", io.BytesIO(b"some image data"), "image/png")},
+            files={"file": ("icon.png", io.BytesIO(ONE_PIXEL_PNG), "image/png")},
         )
 
         assert upload_response.status_code == 200
