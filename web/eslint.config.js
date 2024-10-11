784bd7e8efec6ad0edc21da1d2255897efbf444b
fliiiix <hi@l33t.name>
2024-10-11 09:01:50 +0200

    fix(web): Move to new eslint config
    https://eslint.org/docs/latest/use/configure/migration-guide#packagejson-configuration-no-longer-supported

---
+++ web/eslint.config.js
@@ -0,0 +1,14 @@
+export default [
+    {
+        files: [
+          "./src/**/*.js",
+          "./src/**/*.jsx",
+          "./src/**/*.ts",
+          "./src/**/*.tsx"
+	],
+	extends: [
+          "react-app",
+          "react-app/jest"
+	]
+    }
+];
