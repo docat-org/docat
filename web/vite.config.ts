8de036f01e93072190737709a25577967e12f8ad
fliiiix <hi@l33t.name>
2026-04-10 13:22:22 +0200

    chore(web): Update vite to 8.x

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
